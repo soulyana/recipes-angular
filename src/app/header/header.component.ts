e5a96998f31d0239f90f1f308a02c2b7fe343e02
Soulyana <soulyana.lakew@accenturefederal.com>
2019-08-24 15:19:55 -0400

    creating a recipe model

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.css"]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
