7cd69c8c7c985867578bebac9e174a98939dfcd3
Soulyana <soulyana.lakew@accenturefederal.com>
2019-08-27 11:33:37 -0400

    using a service for cross-component communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  public recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://live.staticflickr.com/975/42193543501_033ce23ef3_b.jpg'),
     new Recipe('Another Test Recipe', 'This is simply a test', 'https://live.staticflickr.com/975/42193543501_033ce23ef3_b.jpg')
   ];
+
+
 
   constructor() { }
 
